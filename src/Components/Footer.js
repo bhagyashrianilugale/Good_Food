11eb9acf45e4b9344bc73b1d3a7fe8b6a1f527c7
bhagyashriugale <bhagyashriugale6970@gmail.com>
2024-07-12 15:03:27 +0530

    Fixed bug

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-10 bottom-0">
+    <footer className="bg-black text-white py-10 mt-auto">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-4 gap-8">
           <div>
